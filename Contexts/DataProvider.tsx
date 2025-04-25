09e6bad598d2e3eebc4b4cd5035881a1ebeac0f4
Target <fastmovingtarget220689@gmail.com>
2025-04-25 16:50:42 +0100

    Adding in useEffects to get each data type on update

--- Contexts/DataProvider.tsx
+++ Contexts/DataProvider.tsx
@@ -3,6 +3,10 @@ import Ingredient from "../Types/Ingredient";
 import Recipe from "../Types/Recipe";
 import Recipe_Plan from "../Types/Recipe_Plan";
 import Shopping_List_Item from "../Types/Shopping_List_Item";
+import { getIngredientsData } from "./Ingredients/GetIngredients";
+import { getRecipesData } from "./Recipes/GetRecipes";
+import { getRecipePlansData } from "./RecipePlans/GetRecipePlans";
+import { getShoppingListData } from "./ShoppingList/GetShoppingList";
 
 const DataContext = createContext({});
 
@@ -13,9 +17,48 @@ const DataProvider = ({children}:{children:React.ReactNode}) => {
     const [recipePlans, setRecipePlans] = useState<Recipe_Plan[]>([])
     const [shoppingList, setShoppingList] = useState<Shopping_List_Item[]>([])
 
+    const databaseProps = {
+        DatabaseServer: process.env.REACT_APP_DATABASE_SERVER || "localhost",
+        DatabasePort: process.env.REACT_APP_DATABASE_PORT || "8080",
+    }
+
+    const userID = 1;
+
     useEffect(() => {
-        
-    }, [])
+        getIngredientsData(
+            databaseProps,
+            userID,
+            ingredients,
+            setIngredients,
+        )
+    }, [ingredients])
+    
+    useEffect(() => {
+        getRecipesData(
+            databaseProps,
+            userID,
+            recipes,
+            setRecipes,
+        )
+    }, [recipes])
+
+    useEffect(() => {
+        getRecipePlansData(
+            databaseProps,
+            userID,
+            recipePlans,
+            setRecipePlans,
+        )
+    }, [recipePlans])
+
+    useEffect(() => {
+        getShoppingListData(
+            databaseProps,
+            userID,
+            shoppingList,
+            setShoppingList,
+        )
+    }, [shoppingList])
 
 
     return (
