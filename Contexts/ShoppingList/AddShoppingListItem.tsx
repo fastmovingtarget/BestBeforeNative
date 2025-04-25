36e31921e75482c5695b58fa98e06c69003a0677
Target <fastmovingtarget220689@gmail.com>
2025-04-25 14:43:01 +0100

    Add Shopping List Item basic test and implementation

--- Contexts/ShoppingList/AddShoppingListItem.tsx
+++ Contexts/ShoppingList/AddShoppingListItem.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 import Shopping_List_Item from "../../Types/Shopping_List_Item";
 
-export const addShoppingListItemData = async () => {
+export const addShoppingListItemData = async (
+    serverProps : {DatabaseServer:string, DatabasePort:string},
+    shoppingList : Shopping_List_Item[],
+    setIngredients : React.Dispatch<React.SetStateAction<Shopping_List_Item[]>>, 
+    shoppingListItem : Shopping_List_Item,
+) => {
 
+    await fetch(
+        `http://${serverProps.DatabaseServer}:${serverProps.DatabasePort}/shoppinglist/`, 
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body : JSON.stringify({
+                ingredient: shoppingListItem,
+            })
+        }
+    ).then((rawData) => {
+        if(!rawData.ok) {
+            return;
+        }
+        rawData.json().then((data) => {//the data returned should be the ingredient that was added including the id
+            setIngredients([
+                ...shoppingList,
+                data
+            ]);
+        })
+    });
 }
--- Types/Shopping_List_Item.tsx
+++ Types/Shopping_List_Item.tsx
@@ -1,11 +1,11 @@
 interface Shopping_List_Item {
-    Item_ID: number;
+    Item_ID?: number;
     Item_Name: string;
     Item_Quantity: number;
-    Item_Recipe_Plan: number | null;
-    Plan_Date: Date | null;
-    Recipe_Name: string | null;
-    Item_Recipe_Plan_Ingredient: number | null; 
+    Item_Recipe_Plan?: number | null;
+    Plan_Date?: Date | null;
+    Recipe_Name?: string | null;
+    Item_Recipe_Plan_Ingredient?: number | null; 
 }
 
 export default Shopping_List_Item;
