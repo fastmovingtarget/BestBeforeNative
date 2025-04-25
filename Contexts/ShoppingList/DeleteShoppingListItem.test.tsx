af3c2b45409378c0fdad0d58993cb9ec19efd99e
Target <fastmovingtarget220689@gmail.com>
2025-04-25 15:08:41 +0100

    Delete Shopping List Item test and code

--- Contexts/ShoppingList/DeleteShoppingListItem.test.tsx
+++ Contexts/ShoppingList/DeleteShoppingListItem.test.tsx
@@ -9,6 +9,80 @@ beforeEach(() => {
     fetch.resetMocks();
 })
 
-test("placeholder", () => {
-    expect(true).toBe(true);
+test('should fetch ingredients data and update state', async () => {
+
+    /*Arrange *******************************************************************/
+    const mockSetShoppingList = jest.fn();
+    const mockServerProps = { DatabaseServer: 'localhost', DatabasePort: '3000' };
+
+    const shoppingList : Shopping_List_Item[] = [
+        {
+            Item_ID: 1,
+            Item_Name: 'Item 1',
+            Item_Quantity: 1,
+        },
+        {
+            Item_ID: 2,
+            Item_Name: 'Item 2',
+            Item_Quantity: 2,
+        },
+    ]; 
+
+    fetch.mockResponseOnce(
+        "",
+        {status: 204, statusText: "Ingredient Deleted"}
+    );
+
+    /*Act **********************************************************************/
+    await deleteShoppingListItemData(
+        mockServerProps,
+        shoppingList,
+        mockSetShoppingList,
+        1
+    );
+
+    /*Assert *******************************************************************/
+
+    expect(mockSetShoppingList).toHaveBeenCalledWith([{
+        Item_ID: 2,
+        Item_Name: 'Item 2',
+        Item_Quantity: 2,
+    }]);
+})
+
+test("should not update state if fetch fails", async () => {
+    
+    /*Arrange *******************************************************************/
+    const mockSetShoppingList = jest.fn();
+    const mockServerProps = { DatabaseServer: 'localhost', DatabasePort: '3000' };
+
+    const shoppingList : Shopping_List_Item[] = [
+        {
+            Item_ID: 1,
+            Item_Name: 'Item 1',
+            Item_Quantity: 1,
+        },
+        {
+            Item_ID: 2,
+            Item_Name: 'Item 2',
+            Item_Quantity: 2,
+        },
+    ]; 
+
+    fetch.mockResponseOnce(
+        "",
+        {status: 500, statusText: "Internal Server Error"}
+    );
+
+    /*Act **********************************************************************/
+    await deleteShoppingListItemData(
+        mockServerProps,
+        shoppingList,
+        mockSetShoppingList,
+        1
+    );
+
+    /*Assert *******************************************************************/
+
+    expect(mockSetShoppingList).not.toHaveBeenCalled();
 })
--- Contexts/ShoppingList/DeleteShoppingListItem.tsx
+++ Contexts/ShoppingList/DeleteShoppingListItem.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import Shopping_List_Item from "../../Types/Shopping_List_Item";
 
-export const deleteShoppingListItemData = async () => {
+export const deleteShoppingListItemData = async (
+    serverProps : {DatabaseServer:string, DatabasePort:string},
+    shoppingList : Shopping_List_Item[],
+    setRecipes : React.Dispatch<React.SetStateAction<Shopping_List_Item[]>>, 
+    shoppingListItem_ID : number,
+) => {
 
+    await fetch(
+        `http://${serverProps.DatabaseServer}:${serverProps.DatabasePort}/shoppingList/${shoppingListItem_ID}`, 
+        {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+    ).then((rawData) => {
+        if(!rawData.ok) {
+            return;
+        }
+        setRecipes(shoppingList.filter((item) => item.Item_ID !== shoppingListItem_ID));//remove the deleted recipe from the list
+    });
 }
