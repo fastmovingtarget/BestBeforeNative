3e80a235662f30f181045310c5aa980b49bf08e3
Target <fastmovingtarget220689@gmail.com>
2025-05-14 15:09:48 +0100

    Post and Put calls now enforce ISO ingredient date formatting

--- Contexts/Ingredients/AddIngredient.tsx
+++ Contexts/Ingredients/AddIngredient.tsx
@@ -3,33 +3,44 @@ import Ingredient from "../../Types/Ingredient";
 
 export const addIngredientData = async (
     serverProps : {DatabaseServer:string, DatabasePort:string},
+    userID : number,
     ingredients : Ingredient[],
     setIngredients : React.Dispatch<React.SetStateAction<Ingredient[]>>, 
     ingredient : Ingredient,
 ) => {
 
-    await fetch(
-        `http://${serverProps.DatabaseServer}:${serverProps.DatabasePort}/ingredients/`, 
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body : JSON.stringify({
-                ingredient: ingredient,
-            })
-        }
-    ).then((rawData) => {
-        if(!rawData.ok) {
-            return;
-        }
-        rawData.json().then((data) => {//the data returned should be the ingredient that was added including the id
-            setIngredients([
-                ...ingredients,
-                {
-                    ...data,
-                    Ingredient_Date: new Date(data.Ingredient_Date),//date comes in as a string and doesn't get properly parsed within .json()
-                }]);
-        })
-    });
+    const updateBody = JSON.stringify({ 
+        ...ingredient,
+        Ingredient_Date: new Date(ingredient.Ingredient_Date || "").toISOString().slice(0, 10), // Format date to YYYY-MM-DD
+        Ingredient_User_ID: userID,
+    })
+
+    let returnPromise = new Promise<"successful" | "failed">((resolve) => {
+        fetch(
+            `http://${serverProps.DatabaseServer}:${serverProps.DatabasePort}/ingredients/`, 
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body : updateBody
+            }
+        ).then((rawData) => {
+            if(!rawData.ok) {
+                resolve("failed");
+            }
+            else{
+                rawData.json().then((data) => {//the data returned should be the ingredient that was added including the id
+                    setIngredients([//we can't add the ingredient to the list until we get the id back from the server
+                        ...ingredients,
+                        {
+                            ...data,
+                            Ingredient_Date: new Date(data.Ingredient_Date),//date comes in as a string and doesn't get properly parsed within .json()
+                        }]);
+                })
+                resolve("successful");
+            }
+        });
+    })
+    return returnPromise;
 }
--- Contexts/Ingredients/UpdateIngredient.tsx
+++ Contexts/Ingredients/UpdateIngredient.tsx
@@ -8,29 +8,36 @@ export const updateIngredientData = async (
     ingredient : Ingredient,
 ) => {
 
-    await fetch(
-        `http://${serverProps.DatabaseServer}:${serverProps.DatabasePort}/ingredients/${ingredient.Ingredient_ID}`, 
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body : JSON.stringify({
-                ingredient: ingredient,
-            })
-        }
-    ).then((rawData) => {
-        if(!rawData.ok) {
-            return;
-        }
-        rawData.json().then((data) => {//the data returned should be the ingredient that was added including the id
-            setIngredients(ingredients.map(element => {
-                if (element.Ingredient_ID !== ingredient.Ingredient_ID)//if the element's ID isn't the input ingredient's then no change
-                    return element;
-                else //otherwise return the ingredient that was input
-                    return ingredient;
-                    
-            }));
-        })
-    });
+    setIngredients(ingredients.map(element => {
+        if (element.Ingredient_ID !== ingredient.Ingredient_ID)//if the element's ID isn't the input ingredient's then no change
+            return element;
+        else //otherwise return the ingredient that was input
+            return ingredient;
+            
+    }));
+
+    const updateBody = JSON.stringify({ 
+        ...ingredient,
+        Ingredient_Date: new Date(ingredient.Ingredient_Date || "").toISOString().slice(0, 10), // Format date to YYYY-MM-DD
+    })
+
+    let returnPromise = new Promise<"successful" | "failed">((resolve) => {
+
+        fetch(
+            `http://${serverProps.DatabaseServer}:${serverProps.DatabasePort}/ingredients/${ingredient.Ingredient_ID}`, 
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body : updateBody
+            }
+        ).then((rawData) => {
+            if(!rawData.ok) {//this should encompass all errors
+                resolve("failed");
+            }
+            else resolve("successful");//nothing useful passed back from the server, so we can just return successful
+        });
+    })
+    return returnPromise;
 }
