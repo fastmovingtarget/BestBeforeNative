caa3a4fd7010971d6a7297a076e69112bab368e0
Target <fastmovingtarget220689@gmail.com>
2025-05-15 15:29:47 +0100

    Consolidating date

--- Contexts/Ingredients/AddIngredient.test.tsx
+++ Contexts/Ingredients/AddIngredient.test.tsx
@@ -14,24 +14,25 @@ test('should fetch ingredients data and update state', async () => {
     /*Arrange *******************************************************************/
     const mockSetIngredients = jest.fn();
     const mockServerProps = { DatabaseServer: 'localhost', DatabasePort: '3000' };
+    const today = new Date();
     const ingredients : Ingredient[] = [
         {
             Ingredient_ID: 1,
             Ingredient_Name: 'Ingredient 1',
-            Ingredient_Date: new Date(),
+            Ingredient_Date: today,
             Ingredient_Quantity: 1,
         },
         {
             Ingredient_ID: 2,
             Ingredient_Name: 'Ingredient 2',
-            Ingredient_Date: new Date(),
+            Ingredient_Date: today,
             Ingredient_Quantity: 2,
         },
     ]; 
 
     const ingredient : Ingredient = {
         Ingredient_Name: 'Ingredient 1',
-        Ingredient_Date: new Date(),
+        Ingredient_Date: today,
         Ingredient_Quantity: 1,
     };
 
@@ -61,7 +62,7 @@ test('should fetch ingredients data and update state', async () => {
             headers: {
                 "Content-Type": "application/json",
             },
-            body : "{\"Ingredient_Name\":\"Ingredient 1\",\"Ingredient_Date\":\"2025-05-14\",\"Ingredient_Quantity\":1,\"Ingredient_User_ID\":1}"
+            body : `{\"Ingredient_Name\":\"Ingredient 1\",\"Ingredient_Date\":\"${today.toISOString().slice(0, 10)}\",\"Ingredient_Quantity\":1,\"Ingredient_User_ID\":1}`
         })
     expect(mockSetIngredients).toHaveBeenCalledWith([
         ...ingredients,
