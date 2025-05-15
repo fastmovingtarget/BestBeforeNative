232066b7cb04018a51d0cbc23f46247f58b4cd16
Target <fastmovingtarget220689@gmail.com>
2025-05-15 15:28:33 +0100

    Added in Recipe Search Option capability, async functionality

--- Contexts/Recipes/GetRecipes.test.tsx
+++ Contexts/Recipes/GetRecipes.test.tsx
@@ -1,65 +1,63 @@
 import { render, screen, act } from '@testing-library/react';
 import { getRecipesData } from './GetRecipes';
-import Recipe from '../../Types/Recipe'; // Adjust the import path as necessary
+import Recipe, {RecipesSearchOptions} from '../../Types/Recipe'; // Adjust the import path as necessary
 import fetchMock from 'jest-fetch-mock';
 fetchMock.enableMocks();
 
+const mockServerProps = { DatabaseServer: 'localhost', DatabasePort: '3000' };
+const userID = 1;
+//         
 // Mocking the fetch function
 beforeEach(() => {
-    fetch.resetMocks();
+    fetchMock.resetMocks();
 })
 
 test('should fetch recipes and add to empty recipes array', async () => {
     const mockSetRecipes = jest.fn();
-    const mockServerProps = { DatabaseServer: 'localhost', DatabasePort: '3000' };
-    const userID = 1;
     const fetchedRecipes : Recipe[] = [
         {
             Recipe_ID: 1,
-            Recipe_Name: 'Added Recipe 1',
+            Recipe_Name: 'Recipe 1',
             Recipe_Difficulty: 1,
             Recipe_Time: 11,
-            Recipe_Instructions: 'Instructions for Added Recipe 1',
+            Recipe_Instructions: 'Instructions for Recipe 1',
             Recipe_Ingredients: [{
                 Recipe_Ingredient_ID: 11,
-                Ingredient_Name: 'Added Recipe 1 Ingredient 1',
+                Ingredient_Name: 'Recipe 1 Ingredient 1',
                 Ingredient_Quantity: 2,
             },
             {
                 Recipe_Ingredient_ID: 12,
-                Ingredient_Name: 'Added Recipe 1 Ingredient 2',
+                Ingredient_Name: 'Recipe 1 Ingredient 2',
                 Ingredient_Quantity: 2,
             }],
         },
         {
             Recipe_ID: 2,
-            Recipe_Name: 'Added Recipe 2',
+            Recipe_Name: 'Recipe 2',
             Recipe_Difficulty: 2,
             Recipe_Time: 22,
-            Recipe_Instructions: 'Instructions for Added Recipe 2',
+            Recipe_Instructions: 'Instructions for Recipe 2',
             Recipe_Ingredients: [{
                 Recipe_Ingredient_ID: 121,
-                Ingredient_Name: 'Added Recipe 2 Ingredient 1',
+                Ingredient_Name: 'Recipe 2 Ingredient 1',
                 Ingredient_Quantity: 2,
             },
             {
                 Recipe_Ingredient_ID: 122,
-                Ingredient_Name: 'Added Recipe 2 Ingredient 2',
+                Ingredient_Name: 'Recipe 2 Ingredient 2',
                 Ingredient_Quantity: 2,
             }],
         }
     ]; // Assuming recipes is an array of Recipe type
 
     //if I instance the dates seperately in the call and response, I get different timestamps 'cos the test takes time, so I'll just make 'em here
-    fetch.mockResponseOnce(JSON.stringify(fetchedRecipes));
+    fetchMock.mockResponseOnce(JSON.stringify(fetchedRecipes));
 
     await getRecipesData(
         mockServerProps,
         userID,
-        [] as Recipe[],
         mockSetRecipes,
-        100,
-        300
     );
 
     expect(mockSetRecipes).toHaveBeenCalledWith([
@@ -68,115 +66,99 @@ test('should fetch recipes and add to empty recipes array', async () => {
     
 })
 
-test('should fetch recipes with amount high and low and update state', async () => {
-    const mockSetRecipes = jest.fn();
-    const mockServerProps = { DatabaseServer: 'localhost', DatabasePort: '3000' };
-    const userID = 1;
-    const fetchedRecipes : Recipe[] = [
-        {
-            Recipe_ID: 1,
-            Recipe_Name: 'Added Recipe 1',
-            Recipe_Difficulty: 1,
-            Recipe_Time: 11,
-            Recipe_Instructions: 'Instructions for Added Recipe 1',
-            Recipe_Ingredients: [{
-                Recipe_Ingredient_ID: 11,
-                Ingredient_Name: 'Added Recipe 1 Ingredient 1',
-                Ingredient_Quantity: 2,
-            },
-            {
-                Recipe_Ingredient_ID: 12,
-                Ingredient_Name: 'Added Recipe 1 Ingredient 2',
-                Ingredient_Quantity: 2,
-            }],
-        },
-        {
-            Recipe_ID: 2,
-            Recipe_Name: 'Added Recipe 2',
-            Recipe_Difficulty: 2,
-            Recipe_Time: 22,
-            Recipe_Instructions: 'Instructions for Added Recipe 2',
-            Recipe_Ingredients: [{
-                Recipe_Ingredient_ID: 121,
-                Ingredient_Name: 'Added Recipe 2 Ingredient 1',
-                Ingredient_Quantity: 2,
-            },
-            {
-                Recipe_Ingredient_ID: 122,
-                Ingredient_Name: 'Added Recipe 2 Ingredient 2',
-                Ingredient_Quantity: 2,
-            }],
-        }
-    ]; // Assuming recipes is an array of Recipe type
-
-    const existingRecipes: Recipe[] = [
-        {
-            Recipe_ID: 1,
-            Recipe_Name: 'Existing Recipe 1',
-            Recipe_Difficulty: 1,
-            Recipe_Time: 11,
-            Recipe_Instructions: 'Instructions for Existing Recipe 1',
-            Recipe_Ingredients: [{
-                Recipe_Ingredient_ID: 11,
-                Ingredient_Name: 'Existing Recipe 1 Ingredient 1',
-                Ingredient_Quantity: 2,
-            },
-            {
-                Recipe_Ingredient_ID: 12,
-                Ingredient_Name: 'Existing Recipe 1 Ingredient 2',
-                Ingredient_Quantity: 2,
-            }],
-        },
-        {
-            Recipe_ID: 2,
-            Recipe_Name: 'Existing Recipe 2',
-            Recipe_Difficulty: 2,
-            Recipe_Time: 22,
-            Recipe_Instructions: 'Instructions for Existing Recipe 2',
-            Recipe_Ingredients: [{
-                Recipe_Ingredient_ID: 121,
-                Ingredient_Name: 'Existing Recipe 2 Ingredient 1',
-                Ingredient_Quantity: 2,
-            },
-            {
-                Recipe_Ingredient_ID: 122,
-                Ingredient_Name: 'Existing Recipe 2 Ingredient 2',
-                Ingredient_Quantity: 2,
-            }],
-        }
-    ]
-
-    //if I instance the dates seperately in the call and response, I get different timestamps 'cos the test takes time, so I'll just make 'em here
-    fetch.mockResponseOnce(JSON.stringify(fetchedRecipes));
-
-    await getRecipesData(
-        mockServerProps,
-        userID,
-        existingRecipes,
-        mockSetRecipes,
-        100,
-        300
-    );
-
-    expect(mockSetRecipes).toHaveBeenCalledWith([
-        ...existingRecipes,
-        ...fetchedRecipes,
-    ]);
-    
-})
 test('should handle fetch error', async () => {
     const mockSetRecipes = jest.fn();
-    const mockServerProps = { DatabaseServer: 'localhost', DatabasePort: '3000' };
-    const userID = 1;
-    const recipes = [] as Recipe[]; // Assuming recipes is an array of Recipe type
-    fetch.mockResponseOnce(JSON.stringify({}), { status: 500 });
+    fetchMock.mockResponseOnce(JSON.stringify({}), { status: 500 });
 
     await getRecipesData(
         mockServerProps,
         userID,
-        recipes,
         mockSetRecipes,
     );
 
     expect(mockSetRecipes).not.toHaveBeenCalled(); // Check if setRecipes was not called
 });
+
+describe("Should be called with options: ", () => {
+    test('search string', async () => {
+        const mockSetRecipes = jest.fn();
+
+        const searchOptions : RecipesSearchOptions = {
+            searchText: 'Recipe 1',
+        }
+
+        const fetchedRecipes : Recipe[] = [// we don't care for the full recipe data, so will just mock a single simple recipe
+            {
+                Recipe_ID: 1,
+                Recipe_Name: 'Recipe 1',
+                Recipe_Difficulty: 1,
+                Recipe_Time: 11,
+                Recipe_Instructions: 'Instructions for Recipe 1',
+                Recipe_Ingredients: [],
+            }
+        ];
+
+        fetchMock.mockResponseOnce(JSON.stringify(fetchedRecipes));
+
+        await getRecipesData(
+            mockServerProps,
+            userID,
+            mockSetRecipes,
+            searchOptions
+        );
+
+        expect(mockSetRecipes).toHaveBeenCalledWith(fetchedRecipes);
+
+        expect(fetchMock).toHaveBeenCalledWith(
+            `http://${mockServerProps.DatabaseServer}:${mockServerProps.DatabasePort}/recipes/${userID}?searchText=Recipe%201`, 
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        )
+    })
+    test('search called with all options', async () => {
+        const mockSetRecipes = jest.fn();
+
+        const searchOptions : RecipesSearchOptions = {
+            searchText: 'Recipe 1',
+            sortBy: 'Recipe_Name',
+            sortOrder: 'asc',
+            amount: 10,            
+        }
+
+        const fetchedRecipes : Recipe[] = [
+            {
+                Recipe_ID: 1,
+                Recipe_Name: 'Recipe 1',
+                Recipe_Difficulty: 1,
+                Recipe_Time: 11,
+                Recipe_Instructions: 'Instructions for Recipe 1',
+                Recipe_Ingredients: [],
+            }
+        ]; 
+
+        fetchMock.mockResponseOnce(JSON.stringify(fetchedRecipes));
+
+        await getRecipesData(
+            mockServerProps,
+            userID,
+            mockSetRecipes,
+            searchOptions
+        );
+
+        expect(mockSetRecipes).toHaveBeenCalledWith(fetchedRecipes);
+
+        expect(fetchMock).toHaveBeenCalledWith(
+            `http://${mockServerProps.DatabaseServer}:${mockServerProps.DatabasePort}/recipes/${userID}?searchText=Recipe%201&sortBy=Recipe_Name&sortOrder=asc&amount=10`, 
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        )
+    })
+})
--- Contexts/Recipes/GetRecipes.tsx
+++ Contexts/Recipes/GetRecipes.tsx
@@ -1,33 +1,39 @@
 import React from "react";
-import Recipe from "../../Types/Recipe";
+import Recipe, {RecipesSearchOptions} from "../../Types/Recipe";
 
 export const getRecipesData = async (serverProps : {DatabaseServer:string, DatabasePort:string},
     userID : number, 
-    recipes : Recipe[],
     setRecipes : React.Dispatch<React.SetStateAction<Recipe[]>>, 
-    startIndex = 0,
-    amount = 200
+    recipesSearchOptions : RecipesSearchOptions = {},
 ) => {
 
-    await fetch(
-        `http://${serverProps.DatabaseServer}:${serverProps.DatabasePort}/recipes/${userID}?startIndex=${startIndex}&amount=${amount}`, 
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }
-    ).then((rawData) => {
-        if(rawData.status !== 200) {
-            return;
-        }
-        rawData.json().then((data) => {
-            setRecipes(
-                [
-                    ...recipes,
-                    ...data
-                ]
-            );
-        })
-    });
+    const optionsString = Object.entries(recipesSearchOptions).map(([key, value]) => {//map the key and uri encoded value pairs to a string joined by &
+        if (value === undefined) return ""; // Skip undefined values
+        return `${key}=${encodeURIComponent(value)}`;
+    }).join("&");
+
+    const returnPromise = new Promise<"updated" | "failed">((resolve, reject) => {
+        fetch(
+            `http://${serverProps.DatabaseServer}:${serverProps.DatabasePort}/recipes/${userID}?${optionsString}`, 
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        ).then((rawData) => {
+            if(rawData.status !== 200) {
+                resolve("failed");
+            }
+            else {
+                rawData.json().then((data) => {
+                    setRecipes(
+                        data
+                    );
+                    resolve("updated");
+                })
+            }
+        });
+    })
+    return returnPromise
 }
