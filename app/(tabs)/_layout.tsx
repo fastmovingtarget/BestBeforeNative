3cbb50b4f4ca840a49636e4831f6ad5ef4fd4686
Target <fastmovingtarget220689@gmail.com>
2025-05-19 15:48:15 +0100

    Adding in le tab for le recipes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="recipes"
+        options={{
+          title: 'recipes',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/recipes.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Image, Platform } from 'react-native';
+
+import RecipesPage from '@/components/Ingredients/IngredientsPage';
+
+export default function RecipesScreen() {
+  return (
+    <RecipesPage />
+  );
+}
+
