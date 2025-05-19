139487687586df0fd9595b0fdbfe2ab276975b14
Target <fastmovingtarget220689@gmail.com>
2025-05-19 17:48:17 +0100

    Recipes tab now calls recipes page rather than ingredients...

--- app/(tabs)/recipes.tsx
+++ app/(tabs)/recipes.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Image, Platform } from 'react-native';
 
-import RecipesPage from '@/components/Ingredients/IngredientsPage';
+import RecipesPage from '@/components/Recipes/RecipesPage';
 
 export default function RecipesScreen() {
   return (
