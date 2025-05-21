907b800332f869bb1345e55c09f5f43b87c378a3
Target <fastmovingtarget220689@gmail.com>
2025-05-21 14:01:27 +0100

    Basic Implementation of list item

---
+++ components/ShoppingList/ShoppingList/ShoppingListItem/ShoppingListItem.test.tsx
@@ -0,0 +1,389 @@
+// Wed May 21 14:03:02 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:03:01 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:03:01 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:03:00 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:59 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:58 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:57 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:57 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:56 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:55 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:55 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:54 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:53 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:53 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:52 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:51 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:50 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:50 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:49 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:48 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:47 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:46 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:45 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:45 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:44 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:43 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:42 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:42 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:41 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:40 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:40 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:39 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:38 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:37 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:37 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:36 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:35 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:34 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:34 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:33 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:32 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:32 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:31 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:30 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:30 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:29 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:28 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:28 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:27 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:26 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:26 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:25 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:24 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:24 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:23 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:22 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:21 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:20 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:20 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:19 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:19 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:18 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:17 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:17 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:16 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:15 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:15 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:14 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:13 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:13 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:12 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:11 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:11 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:10 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:09 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:08 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:07 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:07 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:06 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:06 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:05 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:04 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:03 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:03 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:02 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:01 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:01 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:00 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:59 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:58 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:58 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:57 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:56 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:56 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:55 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:55 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:54 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:53 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:52 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:52 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:51 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:50 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:50 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:49 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:49 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:48 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:47 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:47 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:46 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:45 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:45 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:44 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:43 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:43 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:42 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:41 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:41 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:40 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:40 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:39 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:38 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:38 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:37 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:36 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:35 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:35 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:34 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:33 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:32 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:32 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:31 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:30 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:29 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:29 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:28 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:27 2025 +0100 : Basic Implementation of list item
+
+
+import {render, userEvent} from '@testing-library/react-native';
+import ShoppingListItem from './ShoppingListItem';
+import type Shopping_List_Item from '@/Types/Shopping_List_Item';
+import {useData} from '@/Contexts/DataProvider';
+
+const mockdataContext = {
+  deleteShoppingListItem: jest.fn(),
+};
+
+jest.mock("@/Contexts/DataProvider", () => {
+  return {
+    __esModule: true,
+    useData: jest.fn(),
+  };
+});
+
+const onEditMock = jest.fn();
+
+beforeEach(() => {
+  jest.resetAllMocks();
+  const useDataMock = useData as jest.Mock;
+  useDataMock.mockReturnValue(mockdataContext);
+});
+
+describe('Shopping List Item renders correctly', () => {
+  it('when given all basic item data', () => {
+    const testItemData : Shopping_List_Item = {
+          Item_ID: 1,
+          Item_Name: 'Test Item',
+          Item_Quantity: 1
+    };
+
+
+    const {getByText, queryByText} = render(
+      <ShoppingListItem
+        item={testItemData}
+        onEdit={onEditMock}
+      />,
+    );
+
+    expect(getByText(/Test Item/i)).toBeTruthy();
+    expect(getByText(/1g/i)).toBeTruthy();
+    expect(queryByText(/Buy By/i)).toBeFalsy();
+
+  });
+  it('when given all basic item data and plan data', () => {
+    const testItemData : Shopping_List_Item = {
+          Item_ID: 1,
+          Item_Name: 'Test Item',
+          Item_Quantity: 1,
+          Plan_Date: new Date('2023-10-01'),
+          Item_Recipe_Plan: 1,
+          Recipe_Name: 'Test Recipe',
+          Item_Recipe_Plan_Ingredient: 1,
+    };
+
+    const {getByText} = render(
+      <ShoppingListItem
+        item={testItemData}
+        onEdit={onEditMock}
+      />,
+    );
+
+    expect(getByText(/Test Item/i)).toBeTruthy();
+    expect(getByText(/1g/i)).toBeTruthy();
+    expect(getByText(/01\/10\/2023/i)).toBeTruthy();
+    expect(getByText(/Test Recipe/i)).toBeTruthy();
+  });
+});
+describe('Shopping List Item calls correctly', () => {
+  it("when edit button is pressed", async () => {
+    const user = userEvent.setup();
+    const testItemData : Shopping_List_Item = {
+          Item_ID: 1,
+          Item_Name: 'Test Item',
+          Item_Quantity: 1
+    };
+
+    const {getByText} = render(
+      <ShoppingListItem
+        item={testItemData}
+        onEdit={onEditMock}
+      />,
+    );
+
+    const editButton = getByText(/Edit/i);
+
+    await user.press(editButton);
+
+    expect(onEditMock).toHaveBeenCalledTimes(1);
+    expect(onEditMock).toHaveBeenCalledWith(1);
+  })
+  it("when delete button is pressed", async () => {
+
+    const user = userEvent.setup();
+    const testItemData : Shopping_List_Item = {
+          Item_ID: 1,
+          Item_Name: 'Test Item',
+          Item_Quantity: 1
+    };
+
+    const {getByText} = render(
+      <ShoppingListItem
+        item={testItemData}
+        onEdit={onEditMock}
+      />,
+    );
+
+    const deleteButton = getByText(/Delete/i);
+
+    await user.press(deleteButton);
+
+    expect(mockdataContext.deleteShoppingListItem).toHaveBeenCalledTimes(1);
+    expect(mockdataContext.deleteShoppingListItem).toHaveBeenCalledWith(1);
+  })
+})
---
+++ components/ShoppingList/ShoppingList/ShoppingListItem/ShoppingListItem.tsx
@@ -0,0 +1,301 @@
+// Wed May 21 14:03:02 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:03:01 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:03:01 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:03:00 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:59 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:58 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:57 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:57 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:56 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:55 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:55 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:54 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:53 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:53 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:52 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:51 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:50 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:50 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:49 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:48 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:47 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:46 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:45 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:45 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:44 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:43 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:42 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:42 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:41 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:40 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:40 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:39 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:38 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:37 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:37 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:36 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:35 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:34 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:34 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:33 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:32 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:32 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:31 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:30 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:30 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:29 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:28 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:28 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:27 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:26 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:26 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:25 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:24 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:24 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:23 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:22 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:21 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:20 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:20 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:19 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:19 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:18 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:17 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:17 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:16 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:15 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:15 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:14 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:13 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:13 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:12 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:11 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:11 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:10 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:09 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:08 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:07 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:07 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:06 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:06 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:05 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:04 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:03 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:03 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:02 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:01 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:01 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:02:00 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:59 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:58 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:58 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:57 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:56 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:56 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:55 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:55 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:54 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:53 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:52 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:52 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:51 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:50 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:50 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:49 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:49 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:48 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:47 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:47 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:46 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:45 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:45 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:44 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:43 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:43 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:42 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:41 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:41 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:40 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:40 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:39 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:38 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:38 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:37 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:36 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:35 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:35 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:34 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:33 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:32 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:32 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:31 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:30 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:29 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:29 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:28 2025 +0100 : Basic Implementation of list item
+
+// Wed May 21 14:01:27 2025 +0100 : Basic Implementation of list item
+
+
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import type Shopping_List_Item from "@/Types/Shopping_List_Item";
+import ComponentView from "@/components/CustomComponents/ComponentView";
+import LabelText from "@/components/CustomComponents/LabelText";
+import ButtonView from "@/components/CustomComponents/ButtonView";
+import { useData } from "@/Contexts/DataProvider";
+
+export default function IngredientComponent({ item, onEdit } : { item: Shopping_List_Item, onEdit: (itemID: number) => void }) {
+    const { deleteShoppingListItem } = useData();
+
+    return (
+        <ComponentView >
+            <LabelText>{item.Item_Name}</LabelText>
+            <LabelText>{item.Item_Quantity ? item.Item_Quantity + "g" : "??g"}</LabelText>
+            {item.Plan_Date && item.Recipe_Name ? (
+                <LabelText>Buy By: {item.Plan_Date.toLocaleDateString()} for {item.Recipe_Name}</LabelText>
+            ) : null}
+            <View style={{flexDirection: "row", justifyContent: "space-around", width: "100%"}}>
+                <ButtonView onPress={() => onEdit(item.Item_ID || -1)}>
+                    <LabelText>Edit</LabelText>
+                </ButtonView>
+                <ButtonView onPress={() => {deleteShoppingListItem(item.Item_ID || -1)}}>
+                    <LabelText>Delete</LabelText>
+                </ButtonView>
+            </View>
+        </ComponentView>
+    )
+}
