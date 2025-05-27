77d23b67a1fb735db6dc335037fd48804a3f9f39
Target <fastmovingtarget220689@gmail.com>
2025-05-27 11:24:57 +0100

    Adding Shopping List Search Options

--- Types/Shopping_List_Item.tsx
+++ Types/Shopping_List_Item.tsx
@@ -1,3 +1,5 @@
+//2025-05-27 : Adding Shopping List Search Options
+
 interface Shopping_List_Item {
     Item_ID?: number;
     Item_Name: string;
@@ -7,5 +9,13 @@ interface Shopping_List_Item {
     Recipe_Name?: string | null;
     Item_Recipe_Plan_Ingredient?: number | null; 
 }
+
+export interface ShoppingListSearchOptions {
+    searchString?: string;
+    sortBy?: "Item_Name" | "Item_Quantity";
+    sortOrder?: "asc" | "desc";
+    amount?: number;
+}
+
 
 export default Shopping_List_Item;
