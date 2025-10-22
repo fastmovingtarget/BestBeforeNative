4ceed19519106e2f0e38d62052a98087ea1e5515
Target <fastmovingtarget220689@gmail.com>
2025-10-22 12:47:14 +0100

    Added values to enums to differentiate results

--- Types/DataLoadingState.tsx
+++ Types/DataLoadingState.tsx
@@ -1,15 +1,17 @@
+//2025-10-22 : Added values to enums to differentiate results
+
 //2025-10-20 : Created enumerator to better keep track of what state each context is in
 
 export enum UpdateState {
-    Loading,
-    Failed,
-    Successful,
-    FailedDelete,
-    FailedUpdate,
-    FailedAdd
+    Loading = "UpdateLoading",
+    Failed = "UpdateFailed",
+    Successful = "UpdateSuccessful",
+    FailedDelete = "UpdateFailedDelete",
+    FailedUpdate = "UpdateFailedUpdate",
+    FailedAdd = "UpdateFailedAdd"
 }
 export enum SyncState {
-    Loading,
-    Failed,
-    Successful,
+    Loading = "SyncLoading",
+    Failed = "SyncFailed",
+    Successful = "SyncSuccessful",
 }
