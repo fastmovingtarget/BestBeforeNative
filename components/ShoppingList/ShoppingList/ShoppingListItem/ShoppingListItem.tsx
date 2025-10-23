e9f18b769b56398308bfaa62fb23d7a8b37baa2c
Target <fastmovingtarget220689@gmail.com>
2025-10-23 16:24:41 +0100

    Converted to use Shopping List and Ingredients contexts

--- components/ShoppingList/ShoppingList/ShoppingListItem/ShoppingListItem.tsx
+++ components/ShoppingList/ShoppingList/ShoppingListItem/ShoppingListItem.tsx
@@ -1,17 +1,21 @@
+//2025-10-23 : Converted to use Shopping List and Ingredients contexts
+
 //2025-05-27 : Adding purchase button to the list item
 
 //2025-05-21 : Basic Implementation of list item
 
 
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { View } from "react-native";
 import type Shopping_List_Item from "@/Types/Shopping_List_Item";
 import ComponentView from "@/components/CustomComponents/ComponentView";
 import LabelText from "@/components/CustomComponents/LabelText";
 import ButtonView from "@/components/CustomComponents/ButtonView";
-import { useData } from "@/Contexts/DataProvider";
+import { useShoppingList } from "@/Contexts/ShoppingList/ShoppingListDataProvider";
+import { useIngredients } from "@/Contexts/Ingredients/IngredientsDataProvider";
 
 export default function IngredientComponent({ item, onEdit } : { item: Shopping_List_Item, onEdit: (itemID: number) => void }) {
-    const { deleteShoppingListItem, addIngredient } = useData();
+    const { deleteShoppingItem } = useShoppingList();
+    const { addIngredient } = useIngredients();
 
     const onPurchase = () => {
         if (item.Item_ID) {
@@ -19,7 +23,7 @@ export default function IngredientComponent({ item, onEdit } : { item: Shopping_
                 Ingredient_Name: item.Item_Name,
                 Ingredient_Quantity: item.Item_Quantity || 0,
             });
-            deleteShoppingListItem(item.Item_ID);
+            deleteShoppingItem(item.Item_ID);
         }
     }
 
@@ -37,7 +41,7 @@ export default function IngredientComponent({ item, onEdit } : { item: Shopping_
                 <ButtonView onPress={() => onEdit(item.Item_ID || -1)}>
                     <LabelText>Edit</LabelText>
                 </ButtonView>
-                <ButtonView onPress={() => {deleteShoppingListItem(item.Item_ID || -1)}}>
+                <ButtonView onPress={() => {deleteShoppingItem(item.Item_ID || -1)}}>
                     <LabelText>Delete</LabelText>
                 </ButtonView>
             </View>
