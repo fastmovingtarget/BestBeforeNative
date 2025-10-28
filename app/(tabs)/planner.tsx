9c3d2127fae6906c70a2cdec9d9841bd45c74015
Target <fastmovingtarget220689@gmail.com>
2025-10-28 17:43:14 +0000

    Recipe Plan tab added

---
+++ app/(tabs)/planner.tsx
@@ -0,0 +1,9 @@
+//2025-10-28 : Recipe Plan tab added
+
+import RecipePlanner from '@/components/RecipePlans/RecipePlanner';
+
+export default function RecipesScreen() {
+  return (
+    <RecipePlanner />
+  );
+}   
