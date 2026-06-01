3c6bd0dca37c1a1c6d62dd04ab2c6c896cca5246
Target <fastmovingtarget220689@gmail.com>
2026-06-01 14:42:51 +0100

    Replaced ListView with ScrollableContainer

--- components/Inventory/InventoryList/InventoryList.tsx
+++ components/Inventory/InventoryList/InventoryList.tsx
@@ -1,3 +1,5 @@
+//2026-06-01 : Replaced ListView with ScrollableContainer
+
 //2025-11-21 : Moving common UI elements into their own folder
 
 //2025-11-19 : Renamed "Ingredient(s)" to "Inventory(_Items)"
@@ -9,14 +11,14 @@ import { useInventory } from "@/Contexts/Inventory/InventoryDataProvider";
 import { useState } from "react";
 import InventoryItemComponent from "./InventoryItemComponent/InventoryItemComponent";
 import InventoryItemForm from "../InventoryItemForm/InventoryItemForm";
-import ListView from "@/ui/ListView";
+import { ScrollableContainer} from "@/ui/BestBeforeUI";
 
 export default function IngredientsList({onEdit}: {onEdit: () => void}) {
     const [editId, setEditId] = useState<number | undefined>(undefined);
     const {inventory} = useInventory();
     
     return (
-        <ListView >
+        <ScrollableContainer >
             {inventory.map((inventoryItem: Inventory_Item) => (
                 editId !== inventoryItem.Inventory_Item_ID ?
                     <InventoryItemComponent key={`inventory-item-${inventoryItem.Inventory_Item_ID}`} inventoryItem={inventoryItem} onEdit={(id) => {
@@ -25,6 +27,6 @@ export default function IngredientsList({onEdit}: {onEdit: () => void}) {
                     }}/> :
                     <InventoryItemForm key={`inventory-item-form-${inventoryItem.Inventory_Item_ID}`} inventoryItem={inventoryItem} onCancel={() => {setEditId(undefined)}} isFormVisible={true}/>
             ))}
-        </ListView>
+        </ScrollableContainer>
     );
 }
