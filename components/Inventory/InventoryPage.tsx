dd59e55dac34bbcf93bc93d3f40f2f0f977009e2
Target <fastmovingtarget220689@gmail.com>
2026-06-01 14:46:10 +0100

    FadeComponent with removal

--- components/Inventory/InventoryPage.tsx
+++ components/Inventory/InventoryPage.tsx
@@ -1,3 +1,5 @@
+//2026-06-01 : FadeComponent with removal
+
 //2025-11-21 : Moving common UI elements into their own folder
 
 //2025-11-19 : Renamed "Ingredient(s)" to "Inventory(_Items)"
@@ -5,27 +7,30 @@
 //2025-10-28 : Removing extraneous import
 
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import ButtonView from "../../ui/ButtonView";
-import LabelText from "../../ui/LabelText";
-import PageView from "../../ui/PageView";
+import { StyleSheet } from "react-native";
 import InventorySearch from "./InventorySearch/InventorySearch";
 import InventoryList from "./InventoryList/InventoryList";
 import InventoryItemForm from "./InventoryItemForm/InventoryItemForm";
+import {ButtonView, FadeComponent, LabelText, PageView } from "@/ui/BestBeforeUI";
+import { MountState } from "@/ui/Types/MountState";
 
-export default function IngredientsPage() {
+export default function InventoryPage() {
     const [isFormVisible, setIsFormVisible] = useState(false);
+    const [mountState, setMountState] = useState(MountState.Mount);
 
     return (
         <PageView>
-            <Text>Inventory Page</Text>
             <InventorySearch />
-            <View >
-                <ButtonView accessibilityRole="button" style={isFormVisible ? styles.addInventoryItemInvisible : {margin : 5}} onPress={() => setIsFormVisible(true)}>
-                    <LabelText >Add Inventory Item</LabelText>
-                </ButtonView>
-                <InventoryItemForm onCancel={() => setIsFormVisible(false)} isFormVisible={isFormVisible} />
-            </View>
+            {
+                isFormVisible ? 
+                <InventoryItemForm onCancel={() =>{ setMountState(MountState.Mount); setIsFormVisible(false); }} isFormVisible={isFormVisible} />
+                :
+                <FadeComponent mountState={mountState} onUnmountAnimationEnd={() =>  setIsFormVisible(true) } >
+                    <ButtonView accessibilityRole="button" style={isFormVisible ? styles.addInventoryItemInvisible : {margin : 5}} onPress={() => setMountState(MountState.Unmount)} >
+                        <LabelText >Add Inventory Item</LabelText>
+                    </ButtonView>
+                </FadeComponent>
+            }
             <InventoryList onEdit={() => setIsFormVisible(false)} />
         </PageView>
     );
