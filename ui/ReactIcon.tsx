516e328514a3bae357d1fac72b850372e73c24d2
Target <fastmovingtarget220689@gmail.com>
2026-07-01 11:41:49 +0100

    Adding Icons for Submit and Cancel

--- ui/ReactIcon.tsx
+++ ui/ReactIcon.tsx
@@ -1,3 +1,5 @@
+//2026-07-01 : Adding Icons for Submit and Cancel
+
 //2026-06-30 : Adding more subicons, changing some to MDI for consistency
 
 //2026-06-30 : Adding Icons from material-design-icons
@@ -128,4 +130,43 @@ export function AddRecipeIcon({size = defaultSize, color = Colours.text, backgro
             </RowContainer>
         </RowContainer>
     );    
+}
+
+export function CancelIcon({size = defaultSize, color = Colours.text}: {size?: number, color?: string}) {
+    return (
+        <MaterialDesignIcons name={"backspace-outline"} size={size} color={color} style={{padding:5}} />
+    );  
+}
+
+export function SubmitInventoryIcon({size = defaultSize, color = Colours.text, background = Colours.buttonBackground}: {size?: number, color?: string, background?: string}) {
+    return (
+        <RowContainer style={{padding:0, margin:0, justifyContent:"center", alignItems:"center", position:"relative", width:"auto"}}>
+            <MaterialDesignIcons name={"fridge-outline"} size={size} color={color} style={{padding:5}} />
+            <RowContainer style={{position:"absolute", right:3, bottom:3, padding:0, backgroundColor: background, height:"auto", width:"auto", borderRadius: "50%"}} >
+                <MaterialDesignIcons name={"check-bold"} size={size/2} color={color} />
+            </RowContainer>
+        </RowContainer>
+    );  
+}
+
+export function SubmitRecipeIcon({size = defaultSize, color = Colours.text, background = Colours.buttonBackground}: {size?: number, color?: string, background?: string}) {
+    return (
+        <RowContainer style={{padding:0, margin:0, justifyContent:"center", alignItems:"center", position:"relative", width:"auto"}}>
+            <MaterialDesignIcons name={"pot-steam-outline"} size={size} color={color} style={{padding:5}} />
+            <RowContainer style={{position:"absolute", right:3, bottom:3, padding:0, backgroundColor: background, height:"auto", width:"auto", borderRadius: "50%"}} >
+                <MaterialDesignIcons name={"check-bold"} size={size/2} color={color} />
+            </RowContainer>
+        </RowContainer>
+    );  
+}
+
+export function SubmitShoppingListIcon({size = defaultSize, color = Colours.text, background = Colours.buttonBackground}: {size?: number, color?: string, background?: string}) {
+    return (
+        <RowContainer style={{padding:0, margin:0, justifyContent:"center", alignItems:"center", position:"relative", width:"auto"}}>
+            <MaterialDesignIcons name={"basket-outline"} size={size} color={color} style={{padding:5}} />
+            <RowContainer style={{position:"absolute", right:3, bottom:3, padding:0, backgroundColor: background, height:"auto", width:"auto", borderRadius: "50%"}} >
+                <MaterialDesignIcons name={"check-bold"} size={size/2} color={color} />
+            </RowContainer>
+        </RowContainer>
+    );  
 }
