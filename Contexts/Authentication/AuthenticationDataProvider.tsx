7577714018bc90b0d340496b275bb909c40cd468
Target <fastmovingtarget220689@gmail.com>
2026-07-10 13:30:52 +0100

    Calls for login and signup

--- Contexts/Authentication/AuthenticationDataProvider.tsx
+++ Contexts/Authentication/AuthenticationDataProvider.tsx
@@ -1,11 +1,16 @@
+//2026-07-10 : Calls for login and signup
+
 //2026-06-01 : Holding colour variables in Auth
 
 //2025-10-20 : Created Authentication Context
 
 import { useState, createContext, useContext  } from "react";
+import Login from "./Login";
+import { SyncState } from "@/Types/DataLoadingState";
+import Signup from "./Signup";
 
 const AuthenticationDataContext = createContext({
-    userId: 1 as number,
+    userId: 1 as number | null,
     colours: {
         primary: "#191f2b",
         secondary: "#111111",
@@ -17,11 +22,15 @@ const AuthenticationDataContext = createContext({
         background: string,
         text: string,
     },  
+    setColours: (colours: {primary: string, secondary: string, background: string, text: string}) => {},
+    attemptLogin: (username: string, password: string) => {},
+    logout: () => {},
+    signup: (username: string, password: string) => {},
 });
 
 export const AuthenticationDataProvider = ({children}:{children:React.ReactNode}) => {   
 
-    const [userId] = useState<number>(1);
+    const [userId, setUserId] = useState<number | null>(1);
     const [colours, setColours] = useState<{
         primary: string,
         secondary: string,
@@ -34,10 +43,24 @@ export const AuthenticationDataProvider = ({children}:{children:React.ReactNode}
         text: "#e6e0d4",
     });
 
+    
+
+    const attemptLogin = (username: string, password: string) => {
+        Login(username, password, setUserId)
+    }
+
+    const logout = () => {
+        setUserId(null);
+    }
+    
+    const signup = (username: string, password: string) => {
+        Signup(username, password, setUserId);
+    }
+
     return (
         <AuthenticationDataContext.Provider 
-            value={{userId, colours}}>
-        {children}
+            value={{userId, colours, setColours, attemptLogin, logout, signup}}>
+            {children}
         </AuthenticationDataContext.Provider>
     );
 }
---
+++ Contexts/Authentication/Login.tsx
@@ -0,0 +1,50 @@
+//2026-07-10 : Calls for login and signup
+
+
+import { SyncState } from "@/Types/DataLoadingState";
+import log from "@/utils/log";
+
+export default function Login(username: string, password: string, setUserId: (id: number) => void): Promise<SyncState> {
+
+ const serverProps = {
+        DatabaseServer: process.env.REACT_APP_DATABASE_SERVER || "192.168.50.201",
+        DatabasePort: process.env.REACT_APP_DATABASE_PORT || "5091",
+        DatabaseProtocol: process.env.REACT_APP_PROTOCOL || "http",
+    }
+
+    log(`Logging in: ${username}`, "debug");
+
+    let returnPromise = new Promise<SyncState>((resolve) => {
+        fetch(
+            `${serverProps.DatabaseProtocol}://${serverProps.DatabaseServer}:${serverProps.DatabasePort}/authentication/login`, 
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({Username : username, Password : password}),
+            }
+        ).then((rawData) => {
+            if(rawData.status !== 200) {
+                log(`Error fetching login data: ${rawData.status} : ${rawData.statusText}`, "error");
+                resolve(SyncState.Failed);
+            }
+            else {
+                rawData.json().then((data : {User_ID: number, Token_ID: string, Expiration_Date: string}) => {
+                    if(data.User_ID < 0) {
+                        resolve(SyncState.Failed);
+                    }
+                    else {
+                        setUserId(data.User_ID);
+                        log(`Successfully logged in with user ID: ${data.User_ID}`, "debug");
+                        resolve(SyncState.Successful);
+                    }
+                })
+            }
+        }).catch((error) => {
+            log(`Error fetching login data: ${error}`, "error");
+            resolve(SyncState.Failed);
+        });
+    });
+    return returnPromise;
+}
