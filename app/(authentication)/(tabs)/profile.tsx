c4001c435f684401f449695ead912035ecf0bac4
Target <fastmovingtarget220689@gmail.com>
2026-07-10 13:10:29 +0100

    profile tab to allow sign out (currently testing only)

---
+++ app/(authentication)/(tabs)/profile.tsx
@@ -0,0 +1,9 @@
+//2026-07-10 : profile tab to allow sign out (currently testing only)
+
+import { ProfilePage } from '@/components/Profile/ProfilePage'
+
+export default function Profile() {
+    return (
+        <ProfilePage />
+    )
+}
