934f1eba13e7bfec4e55a9667adf822131f39b91
Target <fastmovingtarget220689@gmail.com>
2026-08-11 16:01:16 +0100

    Improvement to timestamp readability

--- utils/log.ts
+++ utils/log.ts
@@ -6,6 +6,9 @@
  * @returns {void} No return value.
  */
 export default function log(message: string, level: "info" | "warn" | "error" | "debug" = "info") {
-    const timestamp = new Date().toISOString();
-    console[level](`[${timestamp}] ${message}`);
+    const timestamp = new Date();
+    const timeStampDateString = `${timestamp.getFullYear()}-${(timestamp.getMonth() + 1).toString().padStart(2, '0')}-${timestamp.getDate().toString().padStart(2, '0')}`; 
+    const timeStampTimeString = `${timestamp.getHours().toString().padStart(2, '0')}:${timestamp.getMinutes().toString().padStart(2, '0')}:${timestamp.getSeconds().toString().padStart(2, '0')}.${timestamp.getMilliseconds().toString().padStart(3, '0')}`;
+    const timeStampString = `${timeStampDateString} ${timeStampTimeString}`;
+    console[level](`${timeStampString}: ${message}`);
 }
