a158f69d79a58238b479d44b9bbe8b428222246f
Target <fastmovingtarget220689@gmail.com>
2026-09-21 10:29:12 +0100

    added loading state support

--- Contexts/Authentication/AuthenticationDataProvider.tsx
+++ Contexts/Authentication/AuthenticationDataProvider.tsx
@@ -1,14 +1,13 @@
+//2026-09-21 : added loading state support
 //2026-09-15 : Removed colour data handling
 //2026-07-16 : Added function descriptions
 //2026-07-13 : Saving and loading token in Secure Store
-
 //2026-07-10 : Calls for login and signup
-
 //2026-06-01 : Holding colour variables in Auth
-
 //2025-10-20 : Created Authentication Context
 
 import { useState, createContext, useContext, useEffect  } from "react";
+import { UpdateState, SyncState } from "@/Types/DataLoadingState";
 import * as SecureStore from "expo-secure-store";
 import Login from "./Login";
 import Signup from "./Signup";
@@ -20,6 +19,7 @@ import Signup from "./Signup";
  */
 const AuthenticationDataContext = createContext({
     userId: 1 as number | null,
+    loadingDataState: "UpdateLoading" as UpdateState | SyncState,
     attemptLogin: (username: string, password: string) => {},
     logout: () => {},
     signup: (username: string, password: string) => {},
@@ -28,6 +28,30 @@ const AuthenticationDataContext = createContext({
 export const AuthenticationDataProvider = ({children}:{children:React.ReactNode}) => {   
 
     const [userId, setUserId] = useState<number | null>(null);
+    const [loadingDataState, setLoadingDataState] = useState<UpdateState | SyncState>(UpdateState.Loading);
+
+    const saveToken = async (token: string, userId: number) => {
+        await SecureStore.setItemAsync("authToken", JSON.stringify({token, userId}));
+    }
+
+    const getToken = async () => {
+        return await SecureStore.getItemAsync("authToken");
+    }
+
+    const attemptLogin = (username: string, password: string) => {
+        setLoadingDataState(UpdateState.Loading);
+        Login(username, password, setUserId, saveToken).then(() => setLoadingDataState(UpdateState.Successful));
+    }
+
+    const logout = () => {
+        setUserId(null);
+        SecureStore.deleteItemAsync("authToken");
+    }
+    
+    const signup = (username: string, password: string) => {
+        setLoadingDataState(UpdateState.Loading);
+        Signup(username, password, setUserId, saveToken).then(() => setLoadingDataState(UpdateState.Successful));
+    }
 
     useEffect(() => {
         const fetchToken = async () => {
@@ -40,30 +64,9 @@ export const AuthenticationDataProvider = ({children}:{children:React.ReactNode}
         fetchToken();
     }, []);
 
-    const saveToken = async (token: string, userId: number) => {
-        await SecureStore.setItemAsync("authToken", JSON.stringify({token, userId}));
-    }
-
-    const getToken = async () => {
-        return await SecureStore.getItemAsync("authToken");
-    }
-
-    const attemptLogin = (username: string, password: string) => {
-        Login(username, password, setUserId, saveToken)
-    }
-
-    const logout = () => {
-        setUserId(null);
-        SecureStore.deleteItemAsync("authToken");
-    }
-    
-    const signup = (username: string, password: string) => {
-        Signup(username, password, setUserId, saveToken);
-    }
-
     return (
         <AuthenticationDataContext.Provider 
-            value={{userId, attemptLogin, logout, signup}}>
+            value={{userId, loadingDataState, attemptLogin, logout, signup}}>
             {children}
         </AuthenticationDataContext.Provider>
     );
